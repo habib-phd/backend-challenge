0b1e778ae000b11bcb6022f5e2c951f60f166583
Habibullah Rahimi <habibandkh@gmail.com>
2024-03-11 23:38:39 -0600

    Implemented models

--- task_m_s/tasks/models.py
+++ task_m_s/tasks/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Label(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    completion_status = models.BooleanField(default=False)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    labels = models.ManyToManyField(Label)
+
+    def __str__(self):
+        return self.title
